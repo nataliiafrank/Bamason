1352d7e5a8bb472f2769d9ce927138f50a3c7b21
Nataliia Frank <nataliiafrank@gmail.com>
2018-06-09 16:46:57 -0400

    updated schema.sql and bumazonCastomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,9 +14,67 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect(function(err){
-    if(err) throw err;
 
-    console.log('connected!');
-})
+connection.connect(function (err) {
+    if (err) throw err;
+    listProducts();
+});
 
+
+var makeOrder = function () {
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Enter item id that you would like to order?"
+    }, {
+        name: "count",
+        type: "input",
+        message: "How many items would you like to order?"
+    }]).then(function (answer) {
+        var query = "SELECT * FROM products where ?";
+        connection.query(query, {
+            item_id: answer.id
+        }, function (err, res) {
+            if (err) throw err;
+           
+            if (answer.count > res[0].stock_quantity) {
+                console.log('You are trying to order more items than is available in stock');
+                listProducts();
+            } else {
+                processOrder(answer.id, answer.count, res[0].stock_quantity);
+            }
+        });
+    });
+};
+
+
+var listProducts = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log('-- Items avalable in the store --\n');
+        console.log('ID' + ' | ' + ' ITEM ' + ' | ' + 'CATEGORY' + ' | ' + ' PRICE ' + ' | ' + 'QUONTITY');
+        for (i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + ' | ' + res[i].product_name +  ' | ' + res[i].department_name + ' | ' + '$' + res[i].price + ' | ' + res[i].stock_quantity);
+        }
+        makeOrder();
+    });
+};
+
+
+var processOrder = function (id, count, stock_quantity) {
+    var processOrderQuery = "UPDATE products SET ? WHERE ?";
+    connection.query(processOrderQuery, [{
+        stock_quantity: stock_quantity - count
+    }, {
+        item_id: id
+    }], function (err, res) {
+        connection.query("SELECT * FROM products WHERE ?", {
+            item_id: id
+        }, function (err, res) {
+            console.log("You successfully made an order!");
+            console.log("Your total cost is $" + res[0].price * count);
+        });
+        
+    });
+    
+};
--- schema.sql
+++ schema.sql
@@ -5,9 +5,24 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products(
-item_id INTEGER NOT NULL,
-product_name VARCHAR(30) NOT NULL,
+item_id INTEGER AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(50) NOT NULL,
-price INTEGER NULL,
-stock_quantity INTEGER NULL
+price DECIMAL(10,4) NOT NULL,
+stock_quantity INTEGER NOT NULL,
+PRIMARY KEY (item_id)
 );
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("SONY TV", "Electronics", 500, 10),
+("iPhone X", "Electronics", 1200, 5),
+("H&M Dress", "Clothes", 30.99, 22),
+("Michael Kors", "Sweater", 85, 40),
+("Gucci Bag", "Accessories", 8.980, 1),
+("Lip Balm", "Cosmetics", 6.99, 135),
+("Curling Iron", "Beauty", 75.99, 45),
+("Space Jam", "Movies DVD", 5.50, 300),
+("Books", "Hobbit", 24.99, 15),
+("Books", "Wonder", 20, 20);
+
+SELECT * FROM products;
